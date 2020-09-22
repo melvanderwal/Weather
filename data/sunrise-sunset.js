fb88210c0473b0fe2c29cf717022ace38c41226c
melvanderwal <melvanderwal@gmail.com>
2020-09-22 10:01:17 +1000

    Add files via upload

---
+++ data/sunrise-sunset.js
@@ -0,0 +1,18 @@
+var sunriseSunset = {
+    "results": {
+        "sunrise": "2020-09-21T19:37:25+00:00",
+        "sunset": "2020-09-22T07:45:07+00:00",
+        "solar_noon": "2020-09-22T01:41:16+00:00",
+        "day_length": 43662,
+        "civil_twilight_begin": "2020-09-21T19:14:11+00:00",
+        "civil_twilight_end": "2020-09-22T08:08:22+00:00",
+        "nautical_twilight_begin": "2020-09-21T18:47:01+00:00",
+        "nautical_twilight_end": "2020-09-22T08:35:32+00:00",
+        "astronomical_twilight_begin": "2020-09-21T18:19:41+00:00",
+        "astronomical_twilight_end": "2020-09-22T09:02:52+00:00"
+    },
+    "status": "OK",
+    "localDate": "2020-09-22",
+    "sunriseLocal": "2020-09-22T05:37:25.000Z",
+    "sunsetLocal": "2020-09-22T17:45:07.000Z"
+}
