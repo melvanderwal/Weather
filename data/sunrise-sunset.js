1da67103bfb4543827f96b522ec3a3dc638059b0
Mel VanderWal <melvanderwal@gmail.com>
2020-09-23 08:01:55 +1000

    Scheduled sunrise/sunset update

--- data/sunrise-sunset.js
+++ data/sunrise-sunset.js
@@ -1,1 +1,7 @@
-var sunriseSunset={"updated":"23/09/2020, 7:49:06 am","sunriseLocal":"2020-09-23T05:36:16.000+10:00","sunsetLocal":"2020-09-23T17:45:35.000+10:00","sunriseUtc":"2020-09-22T19:36:16+00:00","sunsetUtc":"2020-09-23T07:45:35+00:00"}
+var sunriseSunset={
+	"updated": "23/09/2020, 8:01:55 am",
+	"sunriseLocal": "2020-09-23T05:36:16.000+10:00",
+	"sunsetLocal": "2020-09-23T17:45:35.000+10:00",
+	"sunriseUtc": "2020-09-22T19:36:16+00:00",
+	"sunsetUtc": "2020-09-23T07:45:35+00:00"
+}
