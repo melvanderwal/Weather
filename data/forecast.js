7046d4527ec104eb5fc9e6100a6a8e6c2d0ab966
Mel VanderWal <melvanderwal@gmail.com>
2020-09-24 04:25:43 +1000

    Scheduled forecast update

--- data/forecast.js
+++ data/forecast.js
@@ -1,5 +1,5 @@
 var forecast={
-	"updated": "24/09/2020, 4:10:40 am",
+	"updated": "24/09/2020, 4:25:40 am",
 	"kenmore": [
 		{
 			"class": "day eve",
@@ -80,84 +80,5 @@ var forecast={
 			"description": "Partly cloudy. Slight (20%) chance of a shower, most likely later in the day. Light winds becoming northeasterly 15 to 25 km/h during the morning."
 		}
 	],
-	"ipswich": [
-		{
-			"class": "day eve",
-			"day": "Wednesday",
-			"image": "clear",
-			"summary": "Clear.",
-			"precipPercent": 0,
-			"description": "Clear. Light winds."
-		},
-		{
-			"class": "day main",
-			"day": "Thursday",
-			"image": "sunny",
-			"min": 11,
-			"max": 31,
-			"summary": "Sunny.",
-			"precipPercent": 0,
-			"description": "Sunny. Light winds."
-		},
-		{
-			"class": "day",
-			"day": "Friday",
-			"image": "sunny",
-			"min": 9,
-			"max": 33,
-			"summary": "Sunny.",
-			"precipPercent": 5,
-			"description": "Sunny. Light winds becoming northwesterly 15 to 20 km/h in the early afternoon then becoming light in the late afternoon."
-		},
-		{
-			"class": "day",
-			"day": "Saturday",
-			"image": "sunny",
-			"min": 11,
-			"max": 27,
-			"summary": "Sunny.",
-			"precipPercent": 0,
-			"description": "Sunny. Light winds becoming west to southwesterly 25 to 35 km/h during the morning then becoming light during the evening."
-		},
-		{
-			"class": "day",
-			"day": "Sunday",
-			"image": "sunny",
-			"min": 6,
-			"max": 27,
-			"summary": "Sunny.",
-			"precipPercent": 0,
-			"description": "Sunny. Light winds becoming northeasterly 15 to 20 km/h during the afternoon then becoming light during the evening."
-		},
-		{
-			"class": "day",
-			"day": "Monday",
-			"image": "partly-cloudy",
-			"min": 9,
-			"max": 25,
-			"summary": "Partly cloudy.",
-			"precipPercent": 5,
-			"description": "Partly cloudy. Light winds becoming easterly 15 to 20 km/h during the day."
-		},
-		{
-			"class": "day",
-			"day": "Tuesday",
-			"image": "partly-cloudy",
-			"min": 9,
-			"max": 25,
-			"summary": "Partly cloudy.",
-			"precipPercent": 10,
-			"description": "Partly cloudy. Light winds becoming easterly 15 to 20 km/h during the day."
-		},
-		{
-			"class": "day",
-			"day": "Wednesday",
-			"image": "partly-cloudy",
-			"min": 9,
-			"max": 26,
-			"summary": "Partly cloudy.",
-			"precipPercent": 20,
-			"description": "Partly cloudy. Slight (20%) chance of a shower. Light winds becoming northeasterly 15 to 20 km/h during the day."
-		}
-	]
+	"ipswich": []
 }
