04070ebde1430d4aa20bc4331ddeed6fbfc95384
Mel VanderWal <melvanderwal@gmail.com>
2020-11-05 05:04:54 +1000

    Scheduled forecast update

--- data/forecast.js
+++ data/forecast.js
@@ -1,23 +1,14 @@
 var forecast={
-	"updated": "04/11/2020, 10:49:40 pm",
+	"updated": "05/11/2020, 5:04:40 am",
 	"kenmore": [
-		{
-			"class": "day eve",
-			"day": "Wednesday",
-			"image": "clear",
-			"summary": "Clear.",
-			"precipPercent": 0,
-			"description": "Clear. Winds northeasterly 15 to 20 km/h becoming light in the evening."
-		},
 		{
 			"class": "day main",
 			"day": "Thursday",
 			"image": "storm",
-			"min": 15,
 			"max": 35,
 			"summary": "Possible storm.",
-			"precipPercent": 20,
-			"description": "Partly cloudy. Slight (20%) chance of a shower in the afternoon and evening. The chance of a thunderstorm with little or no rainfall in the afternoon and evening. Light winds becoming northwesterly 25 to 35 km/h in the morning then turning southwesterly 15 to 20 km/h in the evening."
+			"precipPercent": 30,
+			"description": "Partly cloudy. Slight (30%) chance of a shower during this afternoon and evening. The chance of a thunderstorm with little or no rainfall from late this morning possibly with gusty winds. Winds northwesterly 25 to 35 km/h turning southwesterly 15 to 20 km/h in the evening."
 		},
 		{
 			"class": "day",
@@ -83,22 +74,13 @@ var forecast={
 	],
 	"ipswich": [
 		{
-			"class": "day eve",
-			"day": "Wednesday",
-			"image": "clear",
-			"summary": "Clear.",
-			"precipPercent": 0,
-			"description": "Clear. Light winds."
-		},
-		{
 			"class": "day main",
 			"day": "Thursday",
 			"image": "storm",
-			"min": 13,
 			"max": 38,
 			"summary": "Hot. Possible storm.",
 			"precipPercent": 20,
-			"description": "Hot. Partly cloudy. Slight (20%) chance of a shower in the afternoon and evening. The chance of a thunderstorm with little or no rainfall from the late morning. Light winds becoming northwesterly 20 to 30 km/h in the morning then tending northwest to southwesterly 15 to 25 km/h in the late afternoon."
+			"description": "Hot. Partly cloudy. Slight (20%) chance of a shower this afternoon. The chance of a thunderstorm with little or no rainfall from late this morning possibly with gusty winds. Winds northwesterly 20 to 30 km/h turning southwesterly 15 to 20 km/h in the evening."
 		},
 		{
 			"class": "day",
@@ -163,22 +145,13 @@ var forecast={
 	],
 	"warwick": [
 		{
-			"class": "day eve",
-			"day": "Wednesday",
-			"image": "clear",
-			"summary": "Clear.",
-			"precipPercent": 0,
-			"description": "Hot. Clear. Light winds becoming north to northeasterly 15 to 20 km/h in the late evening."
-		},
-		{
 			"class": "day main",
 			"day": "Thursday",
-			"image": "storm",
-			"min": 13,
+			"image": "partly-cloudy",
 			"max": 34,
-			"summary": "Possible storm.",
+			"summary": "Partly cloudy.",
 			"precipPercent": 10,
-			"description": "Hot. Partly cloudy. The chance of a thunderstorm with little or no rainfall from the late morning. Winds northwest to northeasterly 15 to 25 km/h becoming northwesterly 20 to 30 km/h in the morning then turning southwesterly 30 to 45 km/h in the late morning and afternoon. Overnight temperatures falling to between 14 and 20 with daytime temperatures reaching 32 to 38."
+			"description": "Hot. Partly cloudy. The chance of a thunderstorm with little or no rainfall from late this morning. Winds north to northwesterly 20 to 30 km/h shifting southwesterly 35 to 45 km/h in the late morning and afternoon then decreasing to 25 to 35 km/h in the evening. Daytime maximum temperatures 32 to 38."
 		},
 		{
 			"class": "day",
