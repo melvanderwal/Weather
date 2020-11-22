dad61606af2fcd64be5c340c79f21995e369c056
Mel VanderWal <melvanderwal@gmail.com>
2020-11-22 17:49:51 +1000

    Scheduled forecast update

--- data/forecast.js
+++ data/forecast.js
@@ -1,5 +1,5 @@
 var forecast={
-	"updated": "22/11/2020, 5:34:40 pm",
+	"updated": "22/11/2020, 5:49:40 pm",
 	"kenmore": [
 		{
 			"class": "day eve",
@@ -7,7 +7,7 @@ var forecast={
 			"image": "partly-cloudy",
 			"summary": "Mostly sunny.",
 			"precipPercent": 0,
-			"description": "Mostly sunny. Light winds becoming northeasterly 15 to 20 km/h in the late afternoon then becoming light in the evening."
+			"description": "Clear. Winds northeasterly 15 to 20 km/h becoming light in the evening."
 		},
 		{
 			"class": "day main",
@@ -86,16 +86,16 @@ var forecast={
 		{
 			"class": "day eve",
 			"day": "Sunday",
-			"image": "partly-cloudy",
-			"summary": "Mostly sunny.",
+			"image": "clear",
+			"summary": "Clear.",
 			"precipPercent": 0,
-			"description": "Mostly sunny. Light winds."
+			"description": "Clear. Light winds."
 		},
 		{
 			"class": "day main",
 			"day": "Monday",
 			"image": "sunny",
-			"min": 15,
+			"min": 16,
 			"max": 37,
 			"summary": "Hot and sunny.",
 			"precipPercent": 0,
@@ -167,10 +167,10 @@ var forecast={
 		{
 			"class": "day eve",
 			"day": "Sunday",
-			"image": "sunny",
-			"summary": "Sunny.",
+			"image": "clear",
+			"summary": "Clear.",
 			"precipPercent": 0,
-			"description": "Hot and sunny. Winds northeasterly 15 to 25 km/h becoming light in the morning then becoming northeasterly 15 to 20 km/h in the late evening. Daytime maximum temperatures 33 to 39."
+			"description": "Clear. Winds northeasterly 15 to 20 km/h."
 		},
 		{
 			"class": "day main",
@@ -180,7 +180,7 @@ var forecast={
 			"max": 36,
 			"summary": "Hot and sunny.",
 			"precipPercent": 0,
-			"description": "Very hot and mostly sunny. Slight (20%) chance of a shower in the southwest, near zero chance elsewhere. The chance of a thunderstorm in the southwest in the late afternoon and evening. Winds north to northeasterly 15 to 20 km/h tending north to northwesterly 20 to 30 km/h during the morning. Overnight temperatures falling to between 15 and 21 with daytime temperatures reaching 35 to 41."
+			"description": "Very hot. Sunny day. The chance of a thunderstorm in the southwest in the late afternoon and evening. Winds north to northeasterly 15 to 20 km/h tending north to northwesterly 20 to 30 km/h during the morning and early afternoon. Overnight temperatures falling to between 15 and 21 with daytime temperatures reaching 34 to 41."
 		},
 		{
 			"class": "day",
